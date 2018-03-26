1b7acc6a9a13575f713155af2b54918a6abbf418
ideauser <5739728@qq.com>
2018-03-26 11:13:18 +0800

    修改了ch3_17
    修改了ch3_17

--- ch3/ch3_17.cpp
+++ ch3/ch3_17.cpp
@@ -11,15 +11,19 @@ int main()
 	string word;
 	while(cin>>word && word!="q")
 		words.push_back(word);
-
-	for(auto w : words)
+    //注意，下面要使用&来引用，否则修改的是拷贝，而不是在原字符串上修改
+	for(auto &w : words)
 		for(auto &a:w)
 			if(isalpha(a))
-				a=toupper(a);
+            {
+                a=toupper(a);
+                cout<<a;
+            }
+
 
 	for(auto w :words)
 		cout<<w<<endl;
-				
+
 	return 0;
 }
 
